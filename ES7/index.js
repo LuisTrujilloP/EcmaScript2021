7a2fa5e6a31a09c556e804cfe8d6419644e04472
LuisJoseTrujiloP <luisjosetrujillodeveloper@gmail.com>
2022-03-01 23:38:21 -0500

    Finish ES7 March 1st 23:32

---
+++ ES7/index.js
@@ -0,0 +1,17 @@
+// INCLUDES NEW METHOD  if basico con un array
+
+
+let numbers = [1,2,3,4,5,6,7];
+
+
+if (numbers.includes(7)) {
+    console.log('Si se encuentra el valor 7')
+}
+else {
+    console.log('No se encuentra.')
+}
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
